a2e74fff0b5d151d8dc809c5d5c11f650867f0dc
Pan Otlet <otlet0@gmail.com>
2016-02-01 17:29:40 +0100

    Update!

--- js/config.js
+++ js/config.js
@@ -1,54 +1,27 @@
 $(function() {
 
 	CMS.init({
-
-		// Name of your site or location of logo file ,relative to root directory (img/logo.png)
-		siteName: 'My Site',
-
-		// Tagline for your site
-		siteTagline: 'Your site tagline',
-
-		// Email address
-		siteEmail: 'your_email@example.com',
-
-		// Name
-		siteAuthor: 'Your Name',
-
-		// Navigation items
+		siteName: 'CDV News',
+		siteTagline: 'Informatyka zaocznie, 2015-2019',
+		siteEmail: 'potlewski1@edu.cdv.pl',
+		siteAuthor: 'Otlet',
 		siteNavItems: [
-			{ name: 'Github', href: 'https://github.com/yourname', newWindow: false},
-			{ name: 'About'}
+			{ name: 'CDV', href: 'http://cdv.pl', newWindow: true},
+			{ name: 'WU', href: 'http://portal.cdv.pl', newWindow: true},
+			{ name: 'Terminarz'}
 		],
-
-		// Posts folder name
 		postsFolder: 'posts',
-
-		// Homepage posts snippet length
 		postSnippetLength: 120,
-
-		// Pages folder name
 		pagesFolder: 'pages',
-
-		// Site fade speed
 		fadeSpeed: 300,
-
-		// Site footer text
 		footerText: '&copy; ' + new Date().getFullYear() + ' All Rights Reserved.',
-
-		// Mode 'Github' for Github Pages, 'Apache' for Apache server. Defaults
-		// to Github
-		mode: 'Github',
-
-		// If Github mode is set, your Github username and repo name. Defaults
-		// to Github pages branch (gh-pages)
+		mode: 'GitHub',
 		githubUserSettings: {
 			username: 'CDVInformatyka2015',
 			repo: 'info'
 		}
 
 	});
-
-	// Markdown settings
 	marked.setOptions({
 		renderer: new marked.Renderer(),
 		gfm: true,
---
+++ posts/2016-02-01_pierwszypost.md
@@ -0,0 +1,12 @@
+---
+layout: post
+title: Pierwszy post
+---
+
+Z powodu kiepskiej organizacji naszej małej społeczności, problemów z doborem terminów, czy gromadzeniem najważniejszych informacji powstała ta strona.
+
+##Czemu?
+
+Istnieje już grupa na Facebooku, a nawet kilka grup, ale dalej mamy problem z informacjami. Nie ma miejsca, gdzie pojawia się jedna, pełna informacja, pełna terminów, czy materiałów. W końcu mnie, ani nikogo chętnego do pomocy przy rozwoju strony, nie będzie kosztowało jej robienie, niż tylko poświęcenie czasu.
+
+> Więcej informacji wkrótce!
